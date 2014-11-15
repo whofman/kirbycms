689bde20f063f308bb86c64fd957c2397d578d7e
Hans Hofman <hans@hahoweb.nl>
2014-11-15 20:29:45 +0100

    Class Tags added in Sidebar

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -5,7 +5,7 @@
 	 <hr />
 
 	 <div class="widget">
-		 <h3 class="subheader">Recente Artikelen</h3>
+		 <h3 class="subheader"><strong>Recente Artikelen</strong></h3>
  			<?php foreach($pages->find('blog')->children()->visible()->sortBy('date', 'desc')->limit(5) as $article): ?>
 				<h6><a href="<?= $article->url() ?>"><?= html($article->title()) ?></a></h6>
 			<?php endforeach ?>
@@ -14,8 +14,8 @@
 
      <?php $cats = $pages->find('blog')->children()->visible()->sortBy('categories', 'asc')->pluck('categories', ',', true); ?>
 	 <div class="widget">
-		 <h3 class="subheader">Categorieën</h3>
-		 <ul class="taglist">
+		 <h3 class="subheader"><strong>Categorieën</strong></h3>
+		 <ul class="tags">
 	    <?php foreach($cats as $cat): ?>
 	    <li>
 	      <a href="<?php echo $site->url() . '/cat:' . $cat ?>">
@@ -28,8 +28,8 @@
 
      <?php $tags = $pages->find('blog')->children()->visible()->sortBy('tags', 'asc')->pluck('tags', ',', true); ?>
 	 <div class="widget">
-		 <h3 class="subheader">Tags</h3>
-		 <ul>
+		 <h3 class="subheader"><strong>Tags</strong></h3>
+		 <ul class="tags">
 	    <?php foreach($tags as $tag): ?>
 	    <li>
 	      <a href="<?php echo $site->url() . '/tag:' . $tag ?>">
